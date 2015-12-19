446861b9399c50561892282f401adb63925d7da4
Baljot Singh <baljotsingh1986@yahoo.com>
2015-12-18 20:37:46 -0800

    Update Main.java

--- Graph/source/Main.java
+++ Graph/source/Main.java
@@ -30,107 +30,107 @@ public class Main {
 		g.addVertex("L", "LL");
 		g.addVertex("M", "MM");
         
-        System.out.println("\nPrinting graph with vertices have no neighbors.");
+        	System.out.println("\nPrinting graph with vertices have no neighbors.");
 		g.printGraph();
         
-        /*Add neighbors*/
+        	/*Add neighbors*/
 		g.addNeighbour("A", "B", 7);
 		g.addNeighbour("A", "C", 9);
 		g.addNeighbour("A", "F", 14);
 		g.addNeighbour("B", "C", 10);
-        g.addNeighbour("B", "D", 15);
+        	g.addNeighbour("B", "D", 15);
 		g.addNeighbour("C", "D", 11);
 		g.addNeighbour("C", "F", 2);
 		g.addNeighbour("D", "E", 6);
-        g.addNeighbour("E", "F", 9);
-        g.addNeighbour("D", "M", 16);
-        g.addNeighbour("L", "M", 16);
+	 	g.addNeighbour("E", "F", 9);
+        	g.addNeighbour("D", "M", 16);
+        	g.addNeighbour("L", "M", 16);
         
-        System.out.println("\nPrinting graph after adding neighbors.");
+        	System.out.println("\nPrinting graph after adding neighbors.");
 		g.printGraph();
         
-        /*Printing all neighbors of A*/
-        System.out.println("All neighbors of A: " + g.getAllNeighbors("A"));
+        	/*Printing all neighbors of A*/
+        	System.out.println("All neighbors of A: " + g.getAllNeighbors("A"));
         
-        /*get size of graph*/
+        	/*get size of graph*/
 		System.out.println("\nSize: " + g.getSize());
         
-        /*Check edge between two vertices*/
+        	/*Check edge between two vertices*/
 		if (g.isEdge("L", "M") != 0) {
 			System.out.println("There is edge betwen L and M, with weight = " + g.isEdge("L", "M"));
 		}
 		
-        /*Check path between two vertices*/
+        	/*Check path between two vertices*/
 		if (g.isPath("A", "M")) {
 			System.out.println("There is path between A and M.");
 		}
         
-        /*shortest path between A and E*/
+        	/*shortest path between A and E*/
 		System.out.println("\nShortest path between A and E: " + g.shortestPath("A", "E"));
         
-        /*Change the edge weight between B and D*/
-        g.changeEdgeWeight("B", "D", 1);
+        	/*Change the edge weight between B and D*/
+        	g.changeEdgeWeight("B", "D", 1);
         
-        /*shortest path between A and E after changing edge weight between B and D*/
-        System.out.println("\nShortest path between A and E after changing edge weight between B and D: " + g.shortestPath("A", "E"));
+        	/*shortest path between A and E after changing edge weight between B and D*/
+        	System.out.println("\nShortest path between A and E after changing edge weight between B and D: " + g.shortestPath("A", "E"));
 		
-        /*Remove edge between two vertices*/
+        	/*Remove edge between two vertices*/
 		g.removeEdge("D", "M");
 		
-        /*Check if edge is removed*/
+        	/*Check if edge is removed*/
 		if (g.isEdge("D", "M") == 0){
 			System.out.println("Edge is removed between D and M.");
 		}
 		
-        /*Check path after removing the edge*/
+        	/*Check path after removing the edge*/
 		if (!g.isPath("A", "M")){
 			System.out.println("No path between A and M after removing edge between D and M.");
 		}
         
-        /*check path between two vertices*/
+        	/*check path between two vertices*/
 		if (g.isPath("A", "D")){
 			System.out.println("There is path between A and D.");
 		}
         
-        /*Remove vertex B*/
+        	/*Remove vertex B*/
 		g.removeVertex("B");
         
-        /*Remove vertex C*/
+        	/*Remove vertex C*/
 		g.removeVertex("C");
         
-        /*Remove vertex F*/
+        	/*Remove vertex F*/
 		g.removeVertex("F");
         
-        /*Now There is no path after removing B*/
+        	/*Now There is no path after removing B*/
 		if (!g.isPath("A", "D")){
 			System.out.println("No path between A and D after removing B, C and F.");
 		}
         
-        /*Size is changed after removing vertex*/
+        	/*Size is changed after removing vertex*/
 		System.out.println("Size should change: " + g.getSize());
 		
-        /*Check if B is removed*/
+        	/*Check if B is removed*/
 		if (!g.isVertExist("B")){
 			System.out.println("B is deleted.");
 		}
         
-        /*Check if C is removed*/
+        	/*Check if C is removed*/
 		if (!g.isVertExist("C")){
 			System.out.println("C is deleted.");
 		}
         
-        /*Check if D is removed*/
+        	/*Check if D is removed*/
 		if (!g.isVertExist("F")){
 			System.out.println("F is deleted.");
 		}
         
-        /*check if there is edge between B and D*/
+        	/*check if there is edge between B and D*/
 		if (g.isEdge("B", "D") == 0){
 			System.out.println("There is no edge between B and D.");
 		}
         
-        System.out.println("\nPrinting graph after removing vertex B, C, F and edge between D M.");
-        g.printGraph();
+        	System.out.println("\nPrinting graph after removing vertex B, C, F and edge between D M.");
+        	g.printGraph();
         
 		System.out.println("Hello Graph");
 	}
